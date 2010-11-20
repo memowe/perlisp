80b21d553d1817da01d553113cef8d8efda48530
Mirko Westermeier <mirko@westermeier.de>
2010-11-20 14:42:09 +0100

    PerLisp::Base refactoring

--- lib/PerLisp/Base.pm
+++ lib/PerLisp/Base.pm
@@ -1,93 +1,49 @@
 package PerLisp::Base;
-# copy of Mojo::Base - thanks, Sebastian!
+# simplified version of Mojo::Base - thanks, sri!
 
 use strict;
 use warnings;
 
-# No imports because we get subclassed, a lot!
-require Carp;
-
-# Kids, you tried your best and you failed miserably.
-# The lesson is, never try.
+# basic constructor
 sub new {
     my $class = shift;
-
-    # Instantiate
-    return bless
-      exists $_[0] ? exists $_[1] ? {@_} : {%{$_[0]}} : {},
-      ref $class || $class;
+    my $self = exists $_[0] ? exists $_[1] ? {@_} : {%{$_[0]}} : {};
+    return bless $self, ref $class || $class;
 }
 
-# Performance is very important for something as often used as accessors,
-# so we optimize them by compiling our own code, don't be scared, we have
-# tests for every single case
+# accessor generator
 sub attr {
-    my $class   = shift;
-    my $attrs   = shift;
-    my $default = shift;
+    my ($class, $attr, $default) = @_;
 
-    # Check for more arguments
-    Carp::croak('Attribute generator called with too many arguments') if @_;
+    # check attr name
+    die "Attribute '$attr' invalid.\n" unless $attr =~ /^[a-zA-Z_]\w*$/;
 
-    # Shortcut
-    return unless $class && $attrs;
+    # check default
+    die "Default has to be a code reference or constant value.\n"
+        if ref $default && ref $default ne 'CODE';
 
-    # Check default
-    Carp::croak('Default has to be a code reference or constant value')
-      if ref $default && ref $default ne 'CODE';
-
-    # Allow symbolic references
+    # allow symbolic references
     no strict 'refs';
 
-    # Create attributes
-    $attrs = [$attrs] unless ref $attrs eq 'ARRAY';
-    my $ws = '    ';
-    for my $attr (@$attrs) {
-
-        Carp::croak(qq/Attribute "$attr" invalid/)
-          unless $attr =~ /^[a-zA-Z_]\w*$/;
-
-        # Header
-        my $code = "sub {\n";
-
-        # No value
-        $code .= "${ws}if (\@_ == 1) {\n";
-        unless (defined $default) {
-
-            # Return value
-            $code .= "$ws${ws}return \$_[0]->{'$attr'};\n";
+    # create attribute accessor
+    *{"${class}::$attr"} = sub {
+        my $self = shift;
+        if (@_ == 0) { # getter
+            if (defined $default) {
+                return $self->{$attr} if exists $self->{$attr};
+                return $self->{$attr} = ref $default eq 'CODE' ?
+                    $default->($self) : $default;
+            }
+            else {
+                return $self->{$attr};
+            }
         }
-        else {
-
-            # Return value
-            $code .= "$ws${ws}return \$_[0]->{'$attr'} ";
-            $code .= "if exists \$_[0]->{'$attr'};\n";
-
-            # Return default value
-            $code .= "$ws${ws}return \$_[0]->{'$attr'} = ";
-            $code .=
-              ref $default eq 'CODE'
-              ? '$default->($_[0])'
-              : '$default';
-            $code .= ";\n";
+        else { # setter
+            my $new_value  = shift;
+            $self->{$attr} = $new_value;
+            return $self; # method chaining
         }
-        $code .= "$ws}\n";
-
-        # Store value
-        $code .= "$ws\$_[0]->{'$attr'} = \$_[1];\n";
-
-        # Return invocant
-        $code .= "${ws}return \$_[0];\n";
-
-        # Footer
-        $code .= '};';
-
-        # We compile custom attribute code for speed
-        *{"${class}::$attr"} = eval $code;
-
-        # This should never happen (hopefully)
-        Carp::croak("PerLisp::Base compiler error: \n$code\n$@\n") if $@;
-    }
+    };
 }
 
 1;
